e549b7cc010ebf29d6e5b8e710e87f87e99cbb7f
Nathaniel Hudson <nchudson88@gmail.com>
2017-03-11 21:17:58 +0000

    Added additional use case implementations, such as adding new comments/answers on existing posts/questions.

--- nku-net/config/routes.rb
+++ nku-net/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create # Comments ONLY available inside posts.
+  end
+  root 'posts#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +59,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
