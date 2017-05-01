6e9d1c38677adecd85f234227478b9a276a01881
Nathaniel Hudson <nchudson88@gmail.com>
2017-05-01 04:50:19 +0000

    This commit has an updated profile page with a course listing and a functioning post comment form for each post/question on the profile page.

--- db/schema.rb
+++ db/schema.rb
@@ -73,21 +73,9 @@ ActiveRecord::Schema.define(version: 20170427204112) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.integer  "role"
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
