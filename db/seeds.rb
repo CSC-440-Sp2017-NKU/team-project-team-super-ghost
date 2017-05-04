5986a815d580a5555d0d7c0dbce97e454230c236
Shane Jansen <shanejjansen@gmail.com>
2017-05-04 02:15:11 -0400

    Fixed seed to match new models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.create([
+                {name: 'Professor Joe', email: 'teacher@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 3},
+            ])
+
 Course.create([
-                  {title: 'CSC 440'},
-                  {title: 'CSC 362'},
-                  {title: 'CSC 364'},
-                  {title: 'INF 286'}
+                  {title: 'CSC 440', user_id: 1},
+                  {title: 'CSC 362', user_id: 1},
+                  {title: 'CSC 364', user_id: 1},
+                  {title: 'INF 286', user_id: 1}
               ])
 
+# Add teacher to courses
+teacher = User.find(1)
+Course.find_each do |course|
+  teacher.courses << course
+end
+
 User.create([
-                {name: 'Test User 1', email: 'test@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 0},
-                {name: 'Test User 2', email: 'test2@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 0},
+                {name: 'John Smith', email: 'student@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 0},
                 {name: 'admin', email: 'admin@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 1},
                 {name: 'Betsy', email: 'registrar@email.com', password_digest: '$2a$10$ZzjUgLYojB2njCzirZqTT.i6ekk8De8qnfFxLHatdP3LNNBO22a9i', role: 2}
             ]) do |u|
