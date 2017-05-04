b12668125d23842ee88f3cc34367a56e03fe8ee2
Nathaniel Hudson <nchudson88@gmail.com>
2017-05-04 19:55:50 +0000

    Fixed error with CSV upload.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,5 @@
 class Document < ActiveRecord::Base
-    validates :file, :presence => true
+    # validates :file, :presence => true
     def initialize(params = {})
         file = params.delete(:file)
         super
